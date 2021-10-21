2ca10fcb3ea769427aba01dc6c41895896018b80
yin1245 <92633914+yin1245@users.noreply.github.com>
2021-10-21 19:56:49 +0800

    Add files via upload

---
+++ week2/尹祯帅/7-2.cpp
@@ -0,0 +1,38 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int n,m,k,q,i;
+	int c;
+	cin>>n>>m;
+	int a[10001],b[10001];
+	bool jud[n+1],ju;
+	for(i=0;i<m;i++)
+	{
+		cin>>a[i]>>b[i];
+	}
+	cin>>k;
+	for(int j=0;j<k;j++)
+	{
+		ju=1;
+		memset(jud,1,sizeof(jud));
+		cin>>q;
+		for(i=0;i<q;i++)
+		{
+			cin>>c;
+			jud[c]=0;
+		}
+		for(i=0;i<m;i++)
+		{
+			if(jud[a[i]]&&jud[b[i]])
+			{
+				cout<<"NO"<<'\n';
+				ju=0;
+				break;
+			}
+		}
+		if(ju)
+		cout<<"YES"<<'\n';
+	}
+	return 0;
+}
