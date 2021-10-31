e1e733ebd7a59ac4f33a36d4eb3aa513e5837d63
rainylll <93460561+rainylll@users.noreply.github.com>
2021-10-31 13:06:59 +0800

    Create 7-3 归并排序.cpp

---
+++ week2/余乐/7-3 归并排序.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#define N 100001 
+using namespace std;
+int n,c,a[N],b[N];
+void sort(int l,int r){
+	if(l==r) return;
+	int mid=l+(r-l)/2;
+	sort(l,mid);
+	sort(mid+1,r);
+	int len=r-l+1,p1=l,p2=mid+1;
+	for(int i=1;i<=len;i++){
+		if(p1<=mid&&p2<=r) {
+			if(a[p1]<a[p2]) b[i]=a[p1++];
+			else b[i]=a[p2++];
+		}
+		else {
+			if(p1<=mid) b[i]=a[p1++];
+			if(p2<=r) b[i]=a[p2++];
+		}
+	}
+	for(int i=l;i<=r;i++) a[i]=b[i-l+1];
+}
+int main(){
+//	scanf("%d%d",&n,&c);
+	scanf("%d",&n);
+	for(int i=1;i<=n;i++) scanf("%d",&a[i]);
+	sort(1,n);
+	for(int i=1;i<=n;i++){
+		printf("%d",a[i]);
+		printf(" "); 
+	} 
+	return 0;
+}
