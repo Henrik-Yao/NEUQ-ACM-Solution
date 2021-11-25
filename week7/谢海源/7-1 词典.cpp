3b30e2c5133d1153a1d338576a4b194e65cec3fd
Hayden7mi <92671926+Hayden7mi@users.noreply.github.com>
2021-11-25 20:59:33 +0800

    Add files via upload

---
+++ week7/谢海源/7-1 词典.cpp
@@ -0,0 +1,8 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int main(int argc,char *argv[])
+{
+	int sum;
+	if(argc>1);
+    return 0;
+}
---
+++ week7/谢海源/7-4 最长有序子序列.cpp
@@ -0,0 +1,30 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int t, n,dp[1001],a[1001],maxa;
+	cin >> t;
+	while (t--)
+	{
+		maxa = 1;
+		memset(dp, 1, sizeof(dp));
+		memset(a, -1, sizeof(a));
+		cin >> n;
+		for (int i = 1; i <= n; i++)
+		{
+			cin >> a[i];
+		}
+		for (int i = 1; i <= n; i++)
+		{
+			dp[i] = 1;
+			for (int j = i-1; j >= 1; j--)//向前遍历
+			{
+				if (a[i] > a[j]) dp[i] = max(dp[i], dp[j] + 1);
+			}
+			maxa = max(dp[i], maxa);
+		}
+		cout << maxa << endl;
+		if(t>0)cout << endl;
+	}
+	return 0;
+}
