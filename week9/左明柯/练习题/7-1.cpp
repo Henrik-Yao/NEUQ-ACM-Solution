c4395de47f5f6c26d3156c7925ea3f8852c7236d
zmk123456789 <92633279+zmk123456789@users.noreply.github.com>
2021-12-02 23:07:15 +0800

    Create 7-1.cpp

---
+++ week9/左明柯/练习题/7-1.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+unsigned long long n,a[100];
+int main(){
+a[1]=1;
+a[2]=2;
+a[3]=4;
+	for(int i=4;i<=50;i++)
+	a[i]=a[i-1]+a[i-2]+a[i-3];
+	
+	while(cin>>n)
+	cout<<a[n]<<endl;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
