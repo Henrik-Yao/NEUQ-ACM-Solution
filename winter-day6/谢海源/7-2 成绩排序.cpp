2bdcbdd63efdaacbda2bde4bc31576406b4053b3
Hayden7mi <92671926+Hayden7mi@users.noreply.github.com>
2021-12-30 12:35:33 +0800

    Add files via upload

---
+++ winter-day6/谢海源/7-2 成绩排序.cpp
@@ -0,0 +1,26 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+const int maxn = 1e4+5;
+struct stu
+{
+	string name; int mark;
+};
+bool cmp1(stu a, stu b)
+{
+	return a.mark > b.mark;
+}
+bool cmp2(stu a, stu b)
+{
+	return a.mark == b.mark && a.name < b.name;
+}
+int main()
+{
+	stu a[maxn];
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) cin >> a[i].name >> a[i].mark;
+	sort(a, a + n, cmp1);
+	sort(a, a + n, cmp2);
+	for (int i = 0; i < n; i++) cout<< a[i].name <<" "<< a[i].mark<<endl;
+	return 0;
+}
---
+++ winter-day6/谢海源/7-5 堆排序.cpp
@@ -0,0 +1,38 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+const int maxn = 1e5+5;
+void adjust(int a[],int s,int m)
+{
+	int t=a[s];
+	for(int j=2*s;j<=m;j*=2)
+	{
+		if(j<m&&a[j]<a[j+1]) j++;
+		if(t>=a[j]) break;
+		a[s]=a[j];
+		s=j;
+	}
+	a[s]=t;//插入
+}
+void heapsort(int a[],int n)
+{
+	for(int i=n/2;i>=1;i--) adjust(a,i,n);
+	for (int i = 1; i <= n; i++) cout<<a[i]<<" ";//建好堆后输出
+	cout<<endl;
+	for(int i=1;i<=n-1;i++)//取出堆中元素
+	{
+		swap(a[1],a[n+1-i]);//取出根结点放最后
+		adjust(a,1,n-i);//调整堆
+		for(int j=1;j<=n;j++) cout<<a[j]<<" ";
+		cout<<endl;
+	}
+}
+int main()
+{
+	int n;
+	int a[maxn]={};
+	cin >> n;
+	for (int i = 1; i <=n; i++) scanf("%d",&a[i]);
+	
+	heapsort(a,n);
+	return 0;
+}
